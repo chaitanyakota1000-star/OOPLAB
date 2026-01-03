3a9dcda840fb07040e2860104d3612d953a1ff9f
chaitanyakota1000-star <chaitanyakota1000@gmail.com>
2026-01-03 15:08:03 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,27 @@
+class hello{
+	
+	public static void main(String args[]){
+		
+		int a =1000;
+		short b=200;
+		byte c=127;
+		float e =9.7f;
+		double F=20.8;
+		long g=979275L;
+		boolean y=true;
+		boolean x=false;
+		if (y){
+		  int add1=a+b;
+		  System.out.println(add1);
+		}
+		if (x){
+			
+		  double add=e+F;
+		   System.out.println(add);
+		}
+
+        	
+       		
+	}
+}
+
