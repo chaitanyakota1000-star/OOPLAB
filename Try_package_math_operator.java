ba7dfd44adf4f914a3fcad52588673bb48d02f00
chaitanyakota1000-star <chaitanyakota1000@gmail.com>
2026-04-09 14:45:42 +0530

    Add files via upload

---
+++ Try_package_math_operator.java
@@ -0,0 +1,8 @@
+import Math_operator.Addition_package;
+
+class Try_package_math_operator{
+  public static void main(String args[]){
+     Addition_package A=new Addition_package();
+	 A.add(5,10);
+	 }
+  }
